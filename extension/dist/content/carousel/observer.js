d7141468f1ca1fc1a402404a2c5abea051a1b5d2
Dan <dangleeson13@gmail.com>
2026-02-08 14:54:23 +0000

    Delete extension/dist/content/carousel directory

--- extension/dist/content/carousel/observer.js
+++
@@ -1,8 +0,0 @@
-import { addCustomNavItem } from "./YTinjector";
-export const observer = new MutationObserver(() => {
-    addCustomNavItem();
-});
-observer.observe(document.body, {
-    childList: true,
-    subtree: true
-});
