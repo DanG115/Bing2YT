525c1cfdce7a508daa6d195d9ff5b2381a7e343b
Daniel Gleeson <dangleeson13@gmail.com>
2026-02-08 14:01:53 +0000

    Bing2YT_v5.0.0 & Cleanup

--- src/content/welcome.ts
+++
@@ -1,5 +0,0 @@
-document.getElementById('versionNumber').textContent = chrome.runtime.getManifest().version;
-
-document.getElementById('getStartedBtn').addEventListener('click', () => {
-    window.close();
-});
